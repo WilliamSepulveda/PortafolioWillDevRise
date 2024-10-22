2066fe3d57efaa88599f8b285561d332a9d207bd
nn <nn>
2024-10-21 21:36:37 -0500

    feat :tada: se inicia portafolio (willdevrise)
    creando el index con la plantilla principal

---
+++ src/js/index.js
@@ -0,0 +1,30 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const button = document.getElementById("animatedButton");
+    const hoverText = button.querySelector(".front-text");
+    
+    function animateButton() {
+        hoverText.style.width = '100%'; 
+        hoverText.style.filter = 'drop-shadow(0 0 23px #C0C0C0)'; 
+    }
+
+    animateButton();
+    // setTimeout(() => {
+    //     window.location.href = "/src/views/home.html"; 
+    // }, 5500); 
+   
+});
+
+document.addEventListener("DOMContentLoaded", function () {
+
+const cti = document.getElementById("cti");  
+const ctiText = cti.getElementById(".CTI");
+
+// Función para iniciar la animación del texto .CTI
+function animateCTI() {
+    ctiText.style.width = '100%'; 
+    ctiText.style.filter = 'drop-shadow(0 0 23px #6019bd)';
+}
+
+animateCTI();  
+
+});
